ea63e689ab3ca500c89f76a188d0cb7a6afb89e1
skittishtrigger <1intelligentsolution@gmail.com>
2017-02-05 14:41:17 -0500

    base page
    test htm page for inserting js

---
+++ ca_beiber.js
@@ -0,0 +1,39 @@
+
+// Check if the user is ready to play!
+
+confirm("Are you ready to being your adventure?")
+var age = prompt("What is your age?");
+if (age < 13)
+{
+    console.log("Sorry, you are too young to play.");
+}
+else
+{
+    console.log("Welcome to your new world!");
+}
+
+console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
+var userAnswer = prompt("Do you want to race Justin Bieber on stage?'");
+
+if (userAnswer = true)
+{
+    console.log("You aand Bieber start racing. It's neck and neck! You win by a shoelace!");
+}
+else
+{
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+}
+
+var feedBack = prompt("What do you rate this game out of 10?")
+ if (feedBack > 8)
+ 
+{
+    console.log("Thank you! We should race at the next concert!");
+}
+else
+{
+    console.log("I'll keep practising coding and racing");
+};
